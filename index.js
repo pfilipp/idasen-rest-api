31aed63030678d5b83f4e753369bc4e29da2fb29
Piotr Filipp <piotr@filipp.pl>
2020-10-03 17:15:22 +0200

    some initial handling of CLI arguments

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ const restServer = new RestServer(process.env.PORT || '3001', expressServer);
 
 restServer.start();
 
+const cliArgs = process.argv.slice(2);
+console.log(cliArgs);
+
+const handleCLI = async () => {
+  switch(cliArgs[0]){
+    case ('scan'):
+      console.log('starting scan');
+      const devices = await deskManager.getAvailableDevices();
+      console.log(devices);
+      process.exit(0);
+    case ('connect'):
+      await deskManager.connectAsync(cliArgs[1]);
+      break;
+    default:
+      break;
+  }
+}
+
+handleCLI();
+
 process.on('SIGINT', async () => {
   await deskManager.disconnectAsync(deskManager.desk);
   process.exit(0);
